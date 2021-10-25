ec886d3ed266259ea805e587e82723a49d846a06
James Zhang <jameszhanga@gmail.com>
2021-10-24 19:12:10 -0500

    Tic tac toe in python

---
+++ python/game.py
@@ -0,0 +1,17 @@
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+]
+
+
+def display_board():
+    for row in board:
+        print(row)
+
+
+def game():
+    display_board()
+
+
+game()
