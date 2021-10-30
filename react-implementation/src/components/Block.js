bf74a33f8466d74ecd6c6603ee506b548d67afa9
James Zhang <jameszhanga@gmail.com>
2021-10-30 15:21:13 -0500

    check winner complete

--- react-implementation/src/components/Block.js
+++ react-implementation/src/components/Block.js
@@ -1,12 +1,8 @@
 const Block = ({ checked, onClick }) => {
+  const styles = checked ? `block ${checked}` : 'block';
+
   return (
-    <div
-      // className='bg-pink-200 rounded-md
-      // 				text-white font-medium w-24 h-24
-      // 			  justify-items-center items-center '
-      className='block'
-      onClick={onClick}
-    >
+    <div className={styles} onClick={onClick}>
       <p> {checked}</p>
     </div>
   );
--- react-implementation/src/components/Board.js
+++ react-implementation/src/components/Board.js
@@ -1,9 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Block from './Block';
+
+const configs = [
+  [ 0, 1, 2 ],
+  [ 3, 4, 5 ],
+  [ 6, 7, 8 ],
+  [ 0, 3, 6 ],
+  [ 1, 4, 7 ],
+  [ 2, 5, 8 ],
+  [ 0, 4, 8 ],
+  [ 2, 4, 6 ]
+];
 
 const Board = () => {
   const [ game, setGame ] = useState(new Array(9).fill());
   const [ player, setPlayer ] = useState(1);
+
+  useEffect(
+    () => {
+      checkWinner();
+    },
+    [ game ]
+  );
 
   const handleClick = idx => {
     setGame(
@@ -21,6 +39,19 @@ const Board = () => {
         return ele;
       })
     );
+  };
+
+  const checkWinner = () => {
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
+    for (let i = 0; i < configs.length; i++) {
+      const idx = configs[i];
+
+      if (game[idx[0]] && game[idx[0]] === game[idx[1]] && game[idx[0]] === game[idx[2]]) {
+        alert(`player ${player} wins`);
+      }
+    }
   };
 
   return (
